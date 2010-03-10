99ff2474a03bdcdc9195ed022af41f653860f315
oncletom <thomas@oncle-tom.net>
2010-03-10 20:35:54 +0000

    * updating i18n  * adding i18n string for plugin listing
    git-svn-id: http://plugins.svn.wordpress.org/canalblog-importer/trunk@215932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bootstrap.php
+++ bootstrap.php
@@ -4,12 +4,17 @@ Plugin Name: Canalblog Importer
 Description: Enables content importing from a blog hosted on Canalblog platform.
 Author: Oncle Tom
 Version: 1.1-dev
+Text Domain: canalblog-importer
+Domain Path: /i18n
 Author URI: http://case.oncle-tom.net/
 Plugin URI: http://wordpress.org/extend/plugins/canalblog-importer/
 
   This plugin is released under version 3 of the GPL:
   http://www.opensource.org/licenses/gpl-3.0.html
 */
+
+/* needed to enable automatic extraction with Poedit for plugin listing */
+__('Enables content importing from a blog hosted on Canalblog platform.', 'canalblog-importe');
 
 if (phpversion() < '5.1')
 {
