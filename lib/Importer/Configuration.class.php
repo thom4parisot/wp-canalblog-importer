383af3f798c61fc8e910579311de6cccd9c322eb
oncletom <hi@oncletom.io>
2014-08-25 19:01:39 +0200

    Implemented custom domain name check.

--- lib/Importer/Configuration.class.php
+++ lib/Importer/Configuration.class.php
@@ -15,6 +15,32 @@ class CanalblogImporterImporterConfiguration extends CanalblogImporterImporterBa
     return true;
   }
 
+  public function cleanUri($input) {
+    $uri = esc_url_raw(strtolower($input), array('http', 'https'));
+    $parts = parse_url($uri);
+
+    if (!$uri) {
+      return null;
+    }
+
+    return $parts['scheme'] . '://' . $parts['host'];
+  }
+
+  public function assertCanalblogByUri($uri) {
+    return preg_match('#http://[^\.]+.canalblog.com#U', $uri) === 1;
+  }
+
+  public function assertCanalblogByHtml($html) {
+    $dom = $this->getDomDocumentFromHtml($html);
+    $xpath = $xpath = new DOMXPath($dom);
+
+    $result = $xpath->query("//meta[@name='generator']");
+
+    return $result && $result->length && strpos($result->item(0)->getAttribute('content'), 'CanalBlog - http://www.canalblog.com') === 0;
+  }
+
+
+
   /**
    * @see lib/Importer/CanalblogImporterImporterBase#process()
    */
@@ -27,11 +53,10 @@ class CanalblogImporterImporterConfiguration extends CanalblogImporterImporterBa
         return false;
       }
 
-      $uri = esc_url_raw(strtolower($_POST['blog_url']), array('http'));
-      $uri = preg_replace('/(canalblog.com).*$/siU', '\\1', $uri);
+      $uri = $this->cleanUri($_POST['blog_url']);
 
       try{
-        if (preg_match('#http://[^\.]+.canalblog.com#U', $uri) && $this->getRemoteHtml($uri))
+        if ($this->assertCanalblogUri($uri) || $this->assertCanalblogByHtml($this->getRemoteHtml($uri)))
         {
           update_option('canalblog_importer_blog_uri', $uri);
           update_option('canalblog_overwrite_contents', isset($_POST['overwrite_contents']) ? 1 : 0);
