d74bdb3f8485a861641df814d3e85b31f1149103
Thomas Parisot <oncletom@users.noreply.github.com>
2020-05-19 14:48:21 +0200

    Also adjust test bootstrapping
    refs a9e391a2a0c884a63c283d75fcbfa1ba21b640a3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,16 @@
 <?php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
+	exit( 1 );
+}
+
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
@@ -12,9 +18,5 @@ function _manually_load_plugin() {
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-tests_add_filter('wp_die_handler', function () {
-	exit(1);
-});
 
 require $_tests_dir . '/includes/bootstrap.php';
